b511836671ee7f684b54ccd868f21c4d0b26e71c
td <td@technodisaster.com>
2024-07-16 06:05:41 +0530

    chore: add stats

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fmt;
 use std::path::Path;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::sync::Arc;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use tokio::task;
+use tokio::time::Instant;
 use urlencoding::encode;
 use walkdir::WalkDir;
 
@@ -31,6 +35,7 @@ impl fmt::Display for Track {
 
 #[tokio::main]
 async fn main() {
+    let start_time = Instant::now();
     let args: Vec<String> = env::args().collect();
 
     let music_dir = match args.get(1) {
@@ -50,15 +55,29 @@ async fn main() {
         ".mp3", ".flac", ".ogg", ".wav", ".aac", ".m4a", ".wma", ".opus", ".ape",
     ];
 
+    let successful_count = Arc::new(AtomicUsize::new(0));
+    let failed_count = Arc::new(AtomicUsize::new(0));
+
     let walker = WalkDir::new(&music_dir)
         .into_iter()
         .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file() && audio_extensions.iter().any(|&ext| e.path().file_name().unwrap_or_default().to_string_lossy().ends_with(ext)))
+        .filter(|e| {
+            e.file_type().is_file()
+                && audio_extensions.iter().any(|&ext| {
+                    e.path()
+                        .file_name()
+                        .unwrap_or_default()
+                        .to_string_lossy()
+                        .ends_with(ext)
+                })
+        })
         .map(|entry| {
             let music_dir = music_dir.to_owned();
             println!("{}", &entry.path().display());
+            let successful_count = Arc::clone(&successful_count);
+            let failed_count = Arc::clone(&failed_count);
             task::spawn(async move {
-                parse_song_path(&entry.path(), &music_dir).await;
+                parse_song_path(&entry.path(), &music_dir, successful_count, failed_count).await;
             })
         });
 
@@ -67,9 +86,27 @@ async fn main() {
     for task in tasks {
         task.await.unwrap()
     }
+
+    println!(
+        "Successful tasks: {}",
+        successful_count.load(Ordering::SeqCst)
+    );
+    println!("Failed tasks: {}", failed_count.load(Ordering::SeqCst));
+    println!(
+        "Total tasks: {}",
+        successful_count.load(Ordering::SeqCst) + failed_count.load(Ordering::SeqCst)
+    );
+    let end_time = Instant::now();
+    let elapsed_time = end_time.duration_since(start_time);
+    println!("Time taken: {:?}", elapsed_time);
 }
 
-async fn parse_song_path(file_path: &Path, music_dir: &Path) -> Option<(String, String, String)> {
+async fn parse_song_path(
+    file_path: &Path,
+    music_dir: &Path,
+    successful_count: Arc<AtomicUsize>,
+    failed_count: Arc<AtomicUsize>,
+) -> Option<(String, String, String)> {
     if let Some(album_dir) = file_path.parent() {
         if let Some(artist_dir) = album_dir.parent() {
             if let Some(music_dirr) = artist_dir.parent() {
@@ -120,38 +157,55 @@ async fn parse_song_path(file_path: &Path, music_dir: &Path) -> Option<(String, 
                                                 // );
                                                 let file_name = format!(
                                                     "{}/{}/{}/{}.lrc",
-                                                    music_dir.display(),  artist, album, song
+                                                    music_dir.display(),
+                                                    artist,
+                                                    album,
+                                                    song
                                                 );
 
                                                 // Create a new file or overwrite existing one
-                                                let mut file = File::create(&file_name).await.expect(&format!(
-                                                    "Failed to create file {}",
-                                                    file_name
-                                                ));
+                                                let mut file = File::create(&file_name)
+                                                    .await
+                                                    .expect(&format!(
+                                                        "Failed to create file {}",
+                                                        file_name
+                                                    ));
 
                                                 // Write syncedLyrics to the file
                                                 file.write_all(lyrics.as_bytes()).await.expect(
-                                                    &format!("Failed to write to file {}", file_name),
+                                                    &format!(
+                                                        "Failed to write to file {}",
+                                                        file_name
+                                                    ),
                                                 );
 
                                                 println!(
                                                     "Saved lyrics for {} to {}",
                                                     track.name, file_name
                                                 );
+                                                successful_count.fetch_add(1, Ordering::SeqCst);
                                             }
-                                            None => todo!(),
+                                            None => {
+                                                failed_count.fetch_add(1, Ordering::SeqCst);
+                                            }
                                         }
                                     } else {
                                         println!(
                                             "No track with synced lyrics found for {}, found at {}",
-                                            song, file_path.display()
+                                            song,
+                                            file_path.display()
                                         );
+                                        failed_count.fetch_add(1, Ordering::SeqCst);
                                     }
                                 }
-                                Err(_) => todo!(),
+                                Err(_) => {
+                                    failed_count.fetch_add(1, Ordering::SeqCst);
+                                }
                             }
                         }
-                        Err(_) => todo!(),
+                        Err(_) => {
+                            failed_count.fetch_add(1, Ordering::SeqCst);
+                        }
                     }
 
                     return Some((artist, album, song));
