f2cbacdb8987bfc42219a3e014d517190c8294f6
Joel Goguen <jgoguen@jgoguen.ca>
2024-07-25 12:53:47 -0400

    Allow using plain lyrics (#4)
    * Allow using plain lyrics
    
    Allow the option to use plain lyrics if synced aren't available. They
    can be fed into a LRC generator for making proper synced lyrics, and for
    some people plain lyrics is better than no lyrics.
    
    Changes the CLI parser to `clap` to make it easier to declare flags and
    parse the ways someone might pass them. Keeps the existing behaviour as
    default (`lyricsrs /path/to/music/dir` works and skips plain lyrics) and
    adds the `--allow-plain` CLI flag to tell `lyricsrs` to write plain
    lyrics if that's all that's available.
    
    * Update src/main.rs
    
    Co-authored-by: td <td@technodisaster.com>
    
    * Update main.rs
    
    ---------
    
    Co-authored-by: td <td@technodisaster.com>

--- tests/test.rs
+++ tests/test.rs
@@ -1,29 +1,35 @@
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
+use temp_dir::TempDir;
 mod common;
 
 #[tokio::test]
 async fn test_cli() {
-    common::setup().await;
+    // TempDir deletes the created directory when the struct is dropped. Call TempDir::leak() to
+    // keep it around for debugging purposes.
+    let tmpdir = &TempDir::new().unwrap();
+    common::setup(tmpdir).await;
 
     let target_dir = env::var("CARGO_MANIFEST_DIR").expect("could not get target dir");
-    let mut current_dir = env::current_dir().expect("could not get current dir");
-    current_dir.push(common::BASE_DIR);
 
     let mut path = PathBuf::from(target_dir);
-
     path.push("target/release/lyricsrs");
+
     let output = Command::new(path)
-        .arg(current_dir)
+        .arg(tmpdir.path())
         .output()
         .expect("Failed to execute command");
 
-    println!("{:?}", output);
+    let stdout_str = String::from_utf8_lossy(&output.stdout);
+    let stderr_str = String::from_utf8_lossy(&output.stderr);
+
+    println!("Exit code: {}", output.status.code().unwrap());
+    println!("STDOUT: {}", stdout_str);
+    println!("STDERR: {}", stderr_str);
 
     assert!(output.status.success());
 
-    let stdout_str = String::from_utf8_lossy(&output.stdout);
     // keep in sync with SONGS in common/mod.rs
     let to_find = format!(
         "Successful tasks: {}\nFailed tasks: 0\nTotal tasks: {}",
@@ -32,7 +38,43 @@ async fn test_cli() {
     );
     assert!(stdout_str.contains(&to_find));
 
-    assert!(common::check_lrcs().await);
+    assert!(common::check_lrcs(tmpdir).await);
+}
 
-    common::cleanup().await;
+#[tokio::test]
+async fn test_cli_plain_lyrics_allowed() {
+    // TempDir deletes the created directory when the struct is dropped. Call TempDir::leak() to
+    // keep it around for debugging purposes.
+    let tmpdir = &TempDir::new().unwrap();
+    common::setup(tmpdir).await;
+
+    let target_dir = env::var("CARGO_MANIFEST_DIR").expect("could not get target dir");
+
+    let mut path = PathBuf::from(target_dir);
+    path.push("target/release/lyricsrs");
+
+    let output = Command::new(path)
+        .arg("--allow-plain")
+        .arg(tmpdir.path())
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout_str = String::from_utf8_lossy(&output.stdout);
+    let stderr_str = String::from_utf8_lossy(&output.stderr);
+
+    println!("Exit code: {}", output.status.code().unwrap());
+    println!("STDOUT: {:?}", stdout_str);
+    println!("STDERR: {:?}", stderr_str);
+
+    assert!(output.status.success());
+
+    // keep in sync with SONGS in common/mod.rs
+    let to_find = format!(
+        "Successful tasks: {}\nFailed tasks: 0\nTotal tasks: {}",
+        common::SONGS.len(),
+        common::SONGS.len()
+    );
+    assert!(stdout_str.contains(&to_find));
+
+    assert!(common::check_lrcs(tmpdir).await);
 }
